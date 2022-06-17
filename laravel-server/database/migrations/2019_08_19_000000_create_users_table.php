2feb70a86c4e88518abc8a83ba10f90925fbb020
Zakaria <zaktakk96@gmail.com>
2022-06-17 16:42:20 +0300

    Backend - Tested register/login/logout/profile APIs

--- laravel-server/database/migrations/2019_08_19_000000_create_users_table.php
+++ laravel-server/database/migrations/2019_08_19_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->smallInteger(("is_admin"));
+            $table->smallInteger("is_admin")->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
