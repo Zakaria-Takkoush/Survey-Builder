27ce0885ee159d26dbe3332d68bee53756923073
Zakaria <zaktakk96@gmail.com>
2022-06-17 12:23:30 +0300

    Backend - Created Controllers and linked them to routes

--- laravel-server/routes/api.php
+++ laravel-server/routes/api.php
@@ -2,18 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\QyestionController;
+use App\Http\Controllers\Admin\SurveyController;
+use App\Http\Controllers\User\AnswersController;
+use App\Http\Controllers\User\QuestionsController;
+use App\Http\Controllers\User\SurveysController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'v1'], function(){
+    // Route::group(['prefix' => 'user'], function(){
+    //     Route::get('/all_users/{id?}', [UserController::class, 'getAllUsers']);
+    //     Route::post('/register/{user_type_id}', [UserController::class, 'signUp']);
+    // });
 });
