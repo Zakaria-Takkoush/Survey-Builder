6c0d6eba3e622dbd717133d7b34c039e718aa856
Zakaria <zaktakk96@gmail.com>
2022-06-18 20:58:37 +0300

    Frontend - Installed react-router-dom and added basic routing

--- frontend-client/src/App.js
+++ frontend-client/src/App.js
@@ -1,12 +1,30 @@
-import './App.css';
+import "./App.css";
 import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from './pages/Login';
+import Login from "./pages/Login";
+import SurveyPanel from "./pages/admin/SurveyPanel";
+import QuestionsPanel from "./pages/admin/QuestionsPanel";
+import Question from "./pages/survey_list/Question";
+import Surveys from "./pages/survey_list/Surveys";
+import Survey from "./pages/survey_list/Survey";
 
 function App() {
   return (
     <>
-    <Login />
+      <BrowserRouter>
+        <Login />
+
+        <Routes>
+          <Route path="/surv_panel" element={<SurveyPanel />}></Route>
+          <Route
+            path="/surv_panel/questions"
+            element={<QuestionsPanel />}
+          ></Route>
+          <Route path="/surveys" element={<Surveys />}></Route>
+          <Route path="/surveys/survey" element={<Survey />}></Route>
+          <Route path="/surveys/survey/question" element={<Question />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- frontend-client/src/pages/admin/SurveyPanel.jsx
+++ frontend-client/src/pages/admin/SurveyPanel.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react';
 
 const SurveyPanel = () => {
   // Initial State... empty array
