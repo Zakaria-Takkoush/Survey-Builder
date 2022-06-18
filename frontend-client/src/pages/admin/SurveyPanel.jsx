0935771d7cd72461a0c4614bf4a07bf5f91fc83d
Zakaria <zaktakk96@gmail.com>
2022-06-18 21:20:46 +0300

    Frontend - Fetched surveys from backend

--- frontend-client/src/pages/admin/SurveyPanel.jsx
+++ frontend-client/src/pages/admin/SurveyPanel.jsx
@@ -4,9 +4,22 @@ import { useState } from 'react';
 const SurveyPanel = () => {
   // Initial State... empty array
     const [surveys, setSurveys] = useState([]);
+
+    //Fetch All Surveys from Backend
+    async function getSurveys() {
+      const res = await fetch("http://127.0.0.1:8000/api/v1/diaplay_surveys")
+      const data = await res.json();
+      console.log(data)
+      return data;
+    }
+
+    getSurveys()
+
+    // Display Surveys oon load
     
+
   return (
-    <div>SurveyPanel</div>
+    <div></div>
   )
 }
 
