ff97746528c6e50ea2cf5c179f5aa3d477d8d56b
Your Name <zaktakk96@gmail.com>
2022-06-18 09:05:49 +0300

    Backend - Added Button Component

--- frontend-client/src/App.js
+++ frontend-client/src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <>hELLO</>
+    <>
+    <Login />
+    </>
   );
 }
 
---
+++ frontend-client/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Button from '../components/Button'
+
+const Login = () => {
+  return (
+    <><Button text=""></Button></>
+  )
+}
+
+export default Login
