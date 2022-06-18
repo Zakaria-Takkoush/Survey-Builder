346298b356e3e886ff26cc3eeddd8d5b8b2a64a6
Zakaria <zaktakk96@gmail.com>
2022-06-18 21:52:14 +0300

    Frontend - UseEffect for surveys

--- frontend-client/src/pages/admin/SurveyPanel.jsx
+++ frontend-client/src/pages/admin/SurveyPanel.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useEffect } from 'react';
 import { useState } from 'react';
 
 const SurveyPanel = () => {
@@ -6,16 +7,23 @@ const SurveyPanel = () => {
     const [surveys, setSurveys] = useState([]);
 
     //Fetch All Surveys from Backend
-    async function getSurveys() {
+    async function fetchSurveys() {
       const res = await fetch("http://127.0.0.1:8000/api/v1/diaplay_surveys")
       const data = await res.json();
-      console.log(data)
-      return data;
+      return data.surveys;
     }
 
-    getSurveys()
-
     // Display Surveys oon load
+
+    useEffect( () => {
+      async function getSurveys () {
+        const surveys = await fetchSurveys();
+        console.log(surveys);
+        setSurveys(surveys);
+      }
+      getSurveys()
+    }, [])
+
     
 
   return (
