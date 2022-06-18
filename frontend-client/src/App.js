3cff8ebfb2e8afca0e1af30f1939631faccc2ce3
Zakaria <zaktakk96@gmail.com>
2022-06-18 19:15:59 +0300

    Frontend - Added CSS for main pages

--- frontend-client/src/App.js
+++ frontend-client/src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './pages/Login';
 
 function App() {
--- frontend-client/src/pages/admin/SurveyPanel.jsx
+++ frontend-client/src/pages/admin/SurveyPanel.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const SurveyPanel = () => {
+  // Initial State... empty array
+    const [surveys, setSurveys] = useState([]);
+    
   return (
     <div>SurveyPanel</div>
   )
