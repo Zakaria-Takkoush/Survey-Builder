113bfa9e31989d58ea8f0eec3bf8cc3c0084733d
Zakaria <zaktakk96@gmail.com>
2022-06-19 13:29:25 +0300

    Frontend - Added Display Questions useEffect to Users Survey page

--- frontend-client/src/pages/admin/QuestionsPanel.jsx
+++ frontend-client/src/pages/admin/QuestionsPanel.jsx
@@ -6,15 +6,26 @@ const QuestionsPanel = () => {
   // Initial State... empty array of questions
   const [questions, setQuestions] = useState([]);
 
+  // Get the survey_idfrom local storage
+  let survey_id = localStorage.getItem("survey_id")
+
   //Fetch All questions from Backend
   async function fetchQuestions(id) {
-    const res = await fetch(`http://127.0.0.1:8000/api/v1//diaplay_questions/${id}`)
+    const res = await fetch(`http://127.0.0.1:8000/api/v1/diaplay_questions/${id}`)
     const data = await res.json();
-    console.log(data)
-    return data;
+    return data.questions;
   }
-  fetchQuestions()
-  
+
+    // Display Questions of the survey oon load
+
+    useEffect( () => {
+      async function getQuestions () {
+        const questions = await fetchQuestions(survey_id);
+        console.log(questions);
+        setQuestions(questions);
+      }
+      getQuestions()
+    }, [])
 
   return (
     <div>QuestionsPanel</div>
--- frontend-client/src/pages/survey_list/Survey.jsx
+++ frontend-client/src/pages/survey_list/Survey.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
 import Question from './Question'
 
 const Survey = () => {
+
+  // Initial State... empty array of questions
+  const [questions, setQuestions] = useState([]);
+
+  // Get the survey_idfrom local storage
+  let survey_id = localStorage.getItem("survey_id")
+
+  //Fetch All questions from Backend
+  async function fetchQuestions(id) {
+    const res = await fetch(`http://127.0.0.1:8000/api/v1/diaplay_questions/${id}`)
+    const data = await res.json();
+    console.log(data.questions)
+    return data.questions;
+  }
+
+    // Display Questions of the survey oon load
+
+    useEffect( () => {
+      async function getQuestions () {
+        const questions = await fetchQuestions(survey_id);
+        console.log(questions);
+        setQuestions(questions);
+      }
+      getQuestions()
+    }, [])
+
   return (
     <Question/>
   )
