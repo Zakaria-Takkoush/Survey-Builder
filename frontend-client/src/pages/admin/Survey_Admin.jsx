6c7c0f4219961a0c8de8258308bb47aceb44a872
Zakaria <zaktakk96@gmail.com>
2022-06-20 23:16:01 +0300

    Frontend - Added Navigation to Questions Panel

--- frontend-client/src/pages/admin/Survey_Admin.jsx
+++ frontend-client/src/pages/admin/Survey_Admin.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Button from '../../components/Button'
+import { useNavigate } from 'react-router-dom'
 
 const Survey_Admin = ({survey}) => {
+
+  const navigate = useNavigate();
+
   return (
     <div className='survey'>
         <h2>{survey.name}</h2>
         <button className='btn' onClick = {() => {
           console.log(survey.name)
           localStorage.setItem("survey_id", survey.id)
+          navigate('/surv_panel/questions')
         }}>
         Edit Survey
         </button>
